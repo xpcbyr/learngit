e314780f3e7b7270b316550bdface40e8ef8eef8
peter <xpcbyr@sina.com>
2015-11-10 22:02:07 +0800

    add a C-File

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+	int a,b,c;
+	
+	printf("\n Please inpu a,b,c(integer) : ");
+	scanf("%d,%d,%d",&a,&b,&c);
+	
+	printf("\n The sum of a+b+c is: %d \n",a+b+c);
+	
+	return 0;
+}
